f83ac9008611b8bc6eeaca44bc8e5c5563a7da5f
TebanSierra <esteban.sierram@gmail.com>
2019-10-14 23:04:10 -0500

    Refactor codigos serial y paralelo

--- src/paralelo.py
+++ src/paralelo.py
@@ -8,25 +8,50 @@ from textwrap import wrap
 
 CORES = mp.cpu_count()
 
-def counting(long_string):
-    count_a = count_c = count_g = count_t = count_n = 0
-    for i in long_string:
-        if i == 'A':
-            count_a += 1
-        elif i == 'C':
-            count_c += 1
-        elif i == 'G':
-            count_g += 1
-        if i == 'T':
-            count_t += 1
-        else:
-            count_n += 1
 
-    return count_a, count_c, count_g, count_t, count_n
+def counting(long_string):
+    count_a = count_c = count_g = count_t = count_u = count_w = count_s = count_m = count_k = count_r = count_y = count_b = count_d = count_h = count_v = count_n = count_z = 0
+    for i in long_string:
+        if i.upper() == 'A':
+            count_a += 1
+        elif i.upper() == 'C':
+            count_c += 1
+        elif i.upper() == 'G':
+            count_g += 1
+        elif i.upper() == 'T':
+            count_t += 1
+        elif i.upper() == 'U':
+            count_u += 1
+        elif i.upper() == 'W':
+            count_w += 1
+        elif i.upper() == 'S':
+            count_s += 1
+        elif i.upper() == 'M':
+            count_m += 1
+        elif i.upper() == 'K':
+            count_k += 1
+        elif i.upper() == 'R':
+            count_r += 1
+        elif i.upper() == 'Y':
+            count_y += 1
+        elif i.upper() == 'B':
+            count_b += 1
+        elif i.upper() == 'D':
+            count_d += 1
+        elif i.upper() == 'H':
+            count_h += 1
+        elif i.upper() == 'V':
+            count_v += 1
+        elif i.upper() == 'N':
+            count_n += 1
+        else:
+            count_z += 1
+
+    return count_a, count_c, count_g, count_t, count_u, count_w, count_s, count_m, count_k, count_r, count_y, count_b, count_d, count_h, count_v, count_n, count_z
 
 
 def main():
-    print("Cores de la maquina: %d" % CORES)
+    print("Cores de la maquina: %d \n" % CORES)
     file_name = sys.argv[1]
     for record in SeqIO.parse(file_name, 'fasta'):
         POOL = mp.Pool(CORES)
@@ -38,8 +63,12 @@ def main():
         results = POOL.map(counting, [part for part in string_parts])
         POOL.close()
         total = tuple(map(sum, zip(*results)))
-        print("A: {a} C: {c} G: {g} T: {t}".format(a = total[0], c = total[1], g = total[2], t = total[3]))
-        print("Cantidad de N: {n} \n".format(n=total[4]))
+        print("A: {a} C: {c} G: {g} T: {t}".format(
+            a=total[0], c=total[1], g=total[2], t=total[3]))
+        print("OTRAS BASES")
+        print("U: {u} W: {w} S: {s} M: {m} K: {k} R: {r} Y: {y} B: {b} D: {d} H: {h} V: {v} N: {n} Z: {z} \n".format(
+            u=total[4], w=total[5], s=total[6], m=total[7], k=total[8], r=total[9], y=total[10], b=total[11], d=total[12], h=total[13], v=total[14], n=total[15], z=total[16]))
+
 
 if __name__ == "__main__":
     main()
--- src/serial.py
+++ src/serial.py
@@ -4,20 +4,44 @@ from Bio import SeqIO
 
 
 def counting(long_string):
-    count_a = count_c = count_g = count_t = count_n = 0
+    count_a = count_c = count_g = count_t = count_u = count_w = count_s = count_m = count_k = count_r = count_y = count_b = count_d = count_h = count_v = count_n = count_z = 0
     for i in long_string:
-        if i == 'A':
+        if i.upper() == 'A':
             count_a += 1
-        elif i == 'C':
+        elif i.upper() == 'C':
             count_c += 1
-        elif i == 'G':
+        elif i.upper() == 'G':
             count_g += 1
-        if i == 'T':
+        elif i.upper() == 'T':
             count_t += 1
-        else:
+        elif i.upper() == 'U':
+            count_u += 1
+        elif i.upper() == 'W':
+            count_w += 1
+        elif i.upper() == 'S':
+            count_s += 1
+        elif i.upper() == 'M':
+            count_m += 1
+        elif i.upper() == 'K':
+            count_k += 1
+        elif i.upper() == 'R':
+            count_r += 1
+        elif i.upper() == 'Y':
+            count_y += 1
+        elif i.upper() == 'B':
+            count_b += 1
+        elif i.upper() == 'D':
+            count_d += 1
+        elif i.upper() == 'H':
+            count_h += 1
+        elif i.upper() == 'V':
+            count_v += 1
+        elif i.upper() == 'N':
             count_n += 1
+        else:
+            count_z += 1
 
-    return count_a, count_c, count_g, count_t, count_n
+    return count_a, count_c, count_g, count_t, count_u, count_w, count_s, count_m, count_k, count_r, count_y, count_b, count_d, count_h, count_v, count_n, count_z
 
 
 def main():
@@ -25,9 +49,11 @@ def main():
     for record in SeqIO.parse(file_name, "fasta"):
         print("Longitud de la cadena: %d" % len(record))
         _string = record.seq
-        a, c, g, t, n = counting(_string)
+        a, c, g, t, u, w, s, m, k, r, y, b, d, h, v, n, z = counting(_string)
         print("A: {} C: {} G: {} T: {}".format(a, c, g, t))
-        print("Cantidad de N: %d \n" % n)
+        print("OTRAS BASES")
+        print("U: {} W: {} S: {} M: {} K: {} R: {} Y: {} B: {} D: {} H: {} V: {} N: {} Z: {} \n".format(
+            u, w, s, m, k, r, y, b, d, h, v, n, z))
 
 
 if __name__ == "__main__":
